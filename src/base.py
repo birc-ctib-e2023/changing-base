140b9549fc978e89974d7e5f582ebae2b0da3678
Thomas Mailund <mailund@birc.au.dk>
2022-04-07 10:01:15 +0200

    Removed solution

--- src/base.py
+++ src/base.py
@@ -29,23 +29,4 @@ def change_to_base(n: int, b: int) -> str:
     '1F'
     """
     assert 2 <= b <= 16
-
-    # zero is a special case
-    if n == 0:
-        return '0'
-
-    # dealing with sign (this isn't safe outside of Python!)
-    sign, n = ('', n) if n > 0 else ('-', -n)
-
-    res = []
-    while n:
-        res.append(digits[n % b])
-        # Integer division is always "floor division" in Python. That means
-        # that we cannot get 0 for negative numbers (we will get -1). Other
-        # languages do "truncate division" and there a number in the range
-        # (-1,0] maps to zero. Yet other languages don't specify it (and then
-        # we have to get creative!). Here, we ensure that n and b are
-        # positive; then floor and truncate division is the same, and
-        # when n < b we get zero.
-        n //= b
-    return sign + "".join(res[::-1])
+    return ''  # FIXME: return n in the right base
